b26f275c88f66a2164ae8ecd482c2ad705b488f8
root <root@ubuntu.(none)>
2014-02-23 19:26:19 +0530

    Adding Code Documentation

---
+++ CodeDocV.3/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["f","__autoload()"],["c","Api"],["c","ConfigFactory"],["c","Dispatcher"],["c","ErrorHandler"],["c","Exception"],["c","HttpClient"]];
