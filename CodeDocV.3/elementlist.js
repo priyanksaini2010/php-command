a466ad65339b9cb8bd74547d9268f965f1a31c15
root <root@ubuntu.(none)>
2014-02-27 22:41:10 +0530

    removing old CodeDoc

--- CodeDocV.3/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["f","__autoload()"],["c","Api"],["c","ConfigFactory"],["c","Dispatcher"],["c","ErrorHandler"],["c","Exception"],["c","HttpClient"]];
